123d8f9f940865fdce19f68fedde38c4135e8b3e
ocefpaf <ocefpaf@gmail.com>
2015-09-22 14:57:42 -0300

    Added comments to implement last 2 ODVCs

--- odvc/formulas.py
+++ odvc/formulas.py
@@ -17,13 +17,40 @@ def ocean_double_sigma_coordinate(sigma, depth, z1, z2, a, href, k_c):
     """
     Creates a dimensioned version of ocean double sigma.
 
+    Definition:
+      for k <= k_c
+        z(k,j,i)= sigma(k)*f(j,i)
+      for k > k_c
+        z(k,j,i) = f(j,i) + (sigma(k)-1)*(depth(j,i)-f(j,i))
+        f(j,i) = 0.5*(z1+ z2) + 0.5*(z1-z2)* tanh(2*a/(z1-z2)*(depth(j,i)-href))
+
+    where z(k,j,i) is height, positive upwards, relative to ocean datum
+    (e.g. mean sea level) at gridpoint (k,j,i) , sigma(k) is the dimensionless
+    coordinate at vertical gridpoint (k) for k <= k_c , and depth(j,i) is the
+    distance from ocean datum to sea floor (positive value) at horizontal
+    gridpoint (j,i) . z1 , z2 , a , and href are constants.
+
     """
     raise NotImplemented
 
 
-def ocean_sigma_z_coordinate():
+def ocean_sigma_z_coordinate(sigma, eta, depth, depth_c, nsigma, zlev):
     """
     Creates a dimensioned version of ocean sigma over z.
+
+    Definition:
+        for k <= nsigma
+            z(n,k,j,i) = eta(n,j,i) + sigma(k)*(min(depth_c,depth(j,i))+eta(n,j,i))
+        for k > nsigma
+            z(n,k,j,i) = zlev(k)
+
+        where z(n,k,j,i) is height, positive upwards, relative to ocean datum
+        (e.g. mean sea level) at gridpoint (n,k,j,i) , eta(n,j,i) is the height
+        of the ocean surface, positive upwards, relative to ocean datum at
+        gridpoint (n,j,i) , sigma(k) is the dimensionless coordinate at
+        vertical gridpoint (k) for k <= nsigma , and depth(j,i) is the distance
+        from ocean datum to sea floor (positive value) at horizontal
+        gridpoint (j,i) . Above depth depth_c there are nsigma layers.
 
     """
     raise NotImplemented
